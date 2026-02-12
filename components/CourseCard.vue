45a8a0d97cc1673ca74a8639ae8f14c886626bb3
mincheol <10705min@fixelsoft.com>
2026-02-12 11:26:35 +0900

    feat: 코드 추 가

--- components/CourseCard.vue
+++ components/CourseCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card class="my-card" @click="$emit('click')">
+  <q-card v-if="dummy" class="my-card" @click="$emit('click')">
     <img :src="thumbnail" alt="썸네일" />
 
     <q-card-section>
@@ -7,9 +7,7 @@
       <div class="text-subtitle2 ellipsis text-grey-8">
         {{ subtitle }}
       </div>
-      <div>
-        좋아요 : {{likeCount}}
-      </div>
+
     </q-card-section>
   </q-card>
 </template>
@@ -19,12 +17,11 @@ interface Props {
   thumbnail: string;
   title: string;
   subtitle: string;
+  dummy: boolean;
 }
 
 const { thumbnail, title, subtitle } = defineProps<Props>();
 
-
-const likeCount = 1;
 
 defineEmits<{
   click: [];
